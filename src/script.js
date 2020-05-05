5214e90898c538eb6137f1a044dc6b3c1b966a9e
awexli <awexli@mail.sfsu.edu>
2020-05-04 20:25:31 -0700

    complete
    - refactor index.html to use common card-count-text
    - refactor index.html to use only one dark-theme class
    - refactor global.scss to hold code from main.scss
    - refactor global.scss to have main selector implement top background
    - refactor global.scss to have main selector use flexbox alignment
    - refactor global.scss to have main selector mange theme changes
    - refactor header.scss to import toggle-theme.scss
    - use section-default.scss to import both sections

--- src/script.js
+++ src/script.js
@@ -1,31 +1,19 @@
 const toggle = document.querySelector('.toggle-wrapper');
+const mainTheme = document.querySelector('.dark-theme');
 let isLight = false;
+
 toggle.addEventListener('click', () => {
   if (!isLight) {
-    document.body.style.backgroundColor = 'hsl(0, 0%, 100%)';
-    document.querySelectorAll('.white-text').forEach((e) => {
-      e.style.color = 'hsl(230, 17%, 14%)';
-    });
-    document.querySelectorAll('.sub-text').forEach((e) => {
-      e.style.color = 'hsl(228, 12%, 44%)';
-    });
-    document.querySelectorAll('.card-default').forEach((e) => {
-      e.className = e.className.replace(' card-default', '');
-      e.className += ' card-light';
-    });
     isLight = true;
+    mainTheme.className = mainTheme.className.replace(
+      'dark-theme',
+      'light-theme'
+    );
   } else {
-    document.body.style.backgroundColor = 'hsl(230, 17%, 14%)';
-    document.querySelectorAll('.white-text').forEach((e) => {
-      e.style.color = 'hsl(0, 0%, 100%)';
-    });
-    document.querySelectorAll('.sub-text').forEach((e) => {
-      e.style.color = 'hsl(228, 34%, 66%)';
-    });
-    document.querySelectorAll('.card-light').forEach((e) => {
-      e.className = e.className.replace(' card-light', '');
-      e.className += ' card-default';
-    });
     isLight = false;
+    mainTheme.className = mainTheme.className.replace(
+      'light-theme',
+      'dark-theme'
+    );
   }
 });
